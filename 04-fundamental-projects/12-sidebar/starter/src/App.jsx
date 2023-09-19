10b6ba61b331efdd2b6f9d0e85ca79bff31f32de
bbiijjeett <bijeetnathbtech@gmail.com>
2023-09-19 12:17:47 +0530

    commit9

---
+++ 04-fundamental-projects/12-sidebar/starter/src/App.jsx
@@ -0,0 +1,14 @@
+import Home from './Home';
+import Sidebar from './Sidebar';
+import Modal from './Modal';
+
+const App = () => {
+  return (
+    <>
+      <Home />
+      <Modal />
+      <Sidebar />
+    </>
+  );
+};
+export default App;
